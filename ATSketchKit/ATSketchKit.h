cb1cb7cd424189452cebb127720bbc83e84e4667
Arnaud Thiercelin <athiercelin@icloud.com>
2015-11-26 12:52:30 -0600

    Initial Commit

---
+++ ATSketchKit/ATSketchKit.h
@@ -0,0 +1,19 @@
+//
+//  ATSketchKit.h
+//  ATSketchKit
+//
+//  Created by Arnaud Thiercelin on 11/26/15.
+//  Copyright © 2015 Arnaud Thiercelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATSketchKit.
+FOUNDATION_EXPORT double ATSketchKitVersionNumber;
+
+//! Project version string for ATSketchKit.
+FOUNDATION_EXPORT const unsigned char ATSketchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATSketchKit/PublicHeader.h>
+
+
