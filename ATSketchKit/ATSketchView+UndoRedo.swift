cb71607b07df40f39e07534b153cec40f5d3fbdb
Arnaud Thiercelin <athiercelin@icloud.com>
2016-01-15 07:55:11 -0600

    Undo Redo wireframe

---
+++ ATSketchKit/ATSketchView+UndoRedo.swift
@@ -0,0 +1,20 @@
+//
+//  ATSketchView+UndoRedo.swift
+//  ATSketchKit
+//
+//  Created by Arnaud Thiercelin on 1/15/16.
+//  Copyright © 2016 Arnaud Thiercelin. All rights reserved.
+//
+
+import Foundation
+
+extension ATSketchView {
+	
+	public func undo() {
+		
+	}
+	
+	public func redo() {
+		
+	}
+}
