1912c4f8f747b022b19da0c5d60672fc552d6e3f
Arnaud Thiercelin <athiercelin@icloud.com>
2016-01-16 22:38:24 -0600

    Cleanup the tests.

--- ATSketchKitTests/ATSketchKitTests.swift
+++ ATSketchKitTests/ATSketchKitTests.swift
@@ -20,17 +20,5 @@ class ATSketchKitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+	
 }
